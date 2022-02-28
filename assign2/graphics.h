3284d923a9894656d169dbc633c917e19065708d
Alex Pena <alexpena5635@gmail.com>
2022-02-28 13:23:22 -0800

    Copied code from a1 for a2

---
+++ assign2/graphics.h
@@ -0,0 +1,96 @@
+/* graphics.h
+ */
+#ifndef _GRAPHICS_H_
+#define _GRAPHICS_H_
+
+#include <vector>
+#include <memory>
+#include <iostream>
+
+struct color {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+};
+
+namespace colors {
+	constexpr color BLACK{   0,   0,   0 };
+	constexpr color WHITE{ 255, 255, 255 };
+	constexpr color RED{   255,   0,   0 };
+	constexpr color GREEN{   0, 255,   0 };
+	constexpr color BLUE{    0,   0, 255 };
+}
+
+struct pixel {
+    color c;
+};
+
+// This just represents the device or the pix/bit map
+class Canvas {
+	public:
+    	Canvas( std::size_t w, std::size_t h, color background = colors::WHITE );
+
+    	void SetColor( std::size_t x, std::size_t y, color c );
+    
+    	pixel GetPixel( std::size_t x, std::size_t y ) const;
+   		void SetPixel(  std::size_t x, std::size_t y, pixel c );
+    
+    	void Clear();
+
+    	std::size_t Width()  const { return width; }
+    	std::size_t Height() const { return height; }
+
+	private:
+    	std::size_t width, height;
+    	std::vector<pixel> pixels;
+    	color background;
+};
+
+
+
+void Line( Canvas& c, int x1, int y1, int x2, int y2, color color );
+
+void SaveCanvasToFile( Canvas const& canvas, std::string const& fileName );
+
+const int DIM = 3; // 2 for coords, 1 for homog
+
+// 2D Point struct, with constructor, set function, and print
+struct point2D {
+    double x;
+	double y;
+	double h; // homog
+	point2D() { x=0.0; y=0.0; h=0.0;}
+	point2D(double x1, double y1, double h1=1) {x=x1; y=y1; h=h1;}
+	void set(double x1, double y1, double h1=1) {x=x1; y=y1; h=h1;}
+
+	friend std::ostream& operator<<(std::ostream& os, const point2D& p){
+		return os << "Point\n - x: " << p.x << "\n - y: " << p.y << "\n - z: " << p.h << std::endl;
+	}
+};
+
+
+
+std::shared_ptr<Canvas> InitGraphics(	const int w, const int h, 
+										const double wxmin = -10, const double wymin = -10, const double wxmax = 10, const double wymax = 10, 
+										const double vxmin = -1, const double vymin = -1, const double vxmax = 1, const double vymax = 1
+);
+
+void SetViewport(double x1, double y1, double x2, double y2); // set the viewport "corner" coords (global)
+void SetWindow(double x1, double y1, double x2, double y2);  // set the window "corner" coords (global)
+void GetOrigin(point2D& p);
+void PrintViewport();
+void PrintWindow();
+void ChangeViewport(double xmin, double ymin, double xmax, double ymax); 
+void ChangeWindow(double xmin, double ymin, double xmax, double ymax); 
+
+void WindowToViewport(const point2D &in_vector, point2D& out_vector); 
+void ViewportToPixmap(double canvas_w, double canvas_h, const point2D& in_vector, point2D& out_vector);
+
+void MoveTo2D(double x, double y); // On the canvas space, where the "pen" is moved to
+void DrawTo2D(Canvas &c, color color, double x, double y); // Go from the current spot to (x,y) an draw line
+
+
+#endif
+
+
+
---
+++ assign2/testLine.cpp
@@ -0,0 +1,176 @@
+/* testLine.cpp
+
+CS Linux
+g++ -std=c++1y testLine.cpp canvas.cpp 
+
+https://stackoverflow.com/questions/31965413/compile-c14-code-with-g
+
+macOS:
+c++ -std=c++14 testLine.cpp canvas.cpp 
+
+
+ */
+
+#include <iostream>
+#include <cmath>
+
+#include "graphics.h"
+
+double eq1(double x) { return (3.0 * exp(x * -0.33) * sin(x*3)); }
+double eq2(double x)
+{
+	double num = 3*(x*x) - 12*x - 15;
+	double den = (x*x) - 3*x -10;
+	return num/den;
+}
+double eq3(double x, double mult, double b)
+{
+	double expre = (x*x)*( (b*x) + 0.5 );
+	return mult * pow(expre, 0.5);
+}
+
+void drawAxis(Canvas& pixmap, color color, point2D origin, point2D min,  point2D max);
+void plotTest(Canvas& pixmap, point2D origin);
+void plotEq1(Canvas& pixmap, point2D origin); // equation 1
+void plotEq2(Canvas& pixmap, point2D origin); // equation 2
+void plotEq3(Canvas& pixmap, point2D origin); // equation 2
+
+int main()
+{	
+	const int width = 1000 , height = 1000;
+	std::shared_ptr<Canvas> pixmap = InitGraphics(width, height);
+
+	point2D origin(0, 0);
+
+	// ChangeViewport(-1, -1, 0, 0);
+	// plotTest(*pixmap, origin);
+
+	// ChangeWindow(-2, -4, 12, 6);
+	// ChangeViewport(-1, -1, 1, 1);
+	// plotEq1(*pixmap, origin);
+
+	// ChangeWindow(-6, -30, 6, 30);
+	// ChangeViewport(-1, -1, 1, 1);
+	// plotEq2(*pixmap, origin);
+
+	// ChangeWindow(-5, -10, 10, 10);
+	// ChangeViewport(-1, -1, 1, 1);
+	// plotEq3(*pixmap, origin);
+
+	std::string filename2( "mytest.pbm" );
+	SaveCanvasToFile( *pixmap, filename2 );
+    return 0;
+}
+
+// Draw an x and y axis
+void drawAxis(Canvas& pixmap, color color, point2D origin, point2D min,  point2D max)
+{
+	MoveTo2D(min.x, origin.y);
+	DrawTo2D(pixmap, color, max.x, origin.y);
+	MoveTo2D(origin.x, min.y);
+    DrawTo2D(pixmap, color, origin.x, max.y);
+}
+
+// Test the code from sample plot
+void plotTest(Canvas& pixmap, point2D origin)
+{
+	point2D min(origin.x - 2.5, origin.y);
+	point2D max(origin.x + 2.5, origin.y + 7.5);
+
+	drawAxis(pixmap, colors::BLACK, origin, min, max);
+
+	double x = -2.0;
+	double xInc = 4.0 / 100.0;
+	MoveTo2D(x, exp(-x) + origin.y);
+	for(int i = 0; i <= 100; i++)
+	{
+		x += xInc;
+		DrawTo2D(pixmap, colors::BLUE, x, exp(-x) + origin.y);
+		MoveTo2D(x, exp(-x) + origin.y);
+	}
+	
+}
+
+// Draw the first equation
+void plotEq1(Canvas& pixmap, point2D origin)
+{
+	point2D min(0, -3 + origin.y);
+	point2D max(10, 3 + origin.y);
+
+	drawAxis(pixmap, colors::BLACK, origin, min, max);
+
+	double x = min.x;
+	double xInc = (M_PI/180);
+	double y = eq1(x);
+	MoveTo2D(x, y + origin.y); // y = 3.0e^(-0.33(x))sin(3x)    0 <= x <= 3pi
+	for(x = 0; x <= 3*(M_PI); x += xInc)
+	{
+		DrawTo2D(pixmap, colors::BLUE, x, eq1(x) + origin.y);
+		MoveTo2D(x, eq1(x) + origin.y);
+	}
+}
+
+// Draw the second equation
+void plotEq2(Canvas& pixmap, point2D origin)
+{
+	point2D min(-10, -30);
+	point2D max(10, 30);
+
+	drawAxis(pixmap, colors::BLACK, origin, min, max);
+
+	double x = -6;
+	double xInc = 1.0 / 1000.0;
+	double y = eq2(x);
+	MoveTo2D(x, y);
+	for(x = -6.0; x <= 6.0; x += xInc)
+	{
+		if(fabs(fabs(x) - 2.0) < 0.000001)
+		{
+			x += 0.05;
+			MoveTo2D(x, eq2(x));
+		}
+		DrawTo2D(pixmap, colors::BLUE, x, eq2(x));
+		MoveTo2D(x, eq2(x));
+	}
+	
+}
+
+// Draw the third equation
+void plotEq3(Canvas& pixmap, point2D origin)
+{
+	point2D min(-10, -10);
+	point2D max(10, 10);
+
+	drawAxis(pixmap, colors::BLACK, origin, min, max);
+
+	double x;
+	double xInc = 1.0 / 1000.0;
+
+	// const double b = 0.5;
+	// x = -1;
+
+	// const double b = 1;
+	// x = -0.5;
+
+	const double b = 2;
+	x = -0.25;
+
+	double y = eq3(x, 1, b);
+	MoveTo2D(x, y);
+	for(x; x <= 50.0; x += xInc)
+	{
+		DrawTo2D(pixmap, colors::BLUE, x, eq3(x, 1, b));
+		MoveTo2D(x, eq3(x, 1, b));
+	}
+
+	x = -0.25;
+	y = eq3(x, -1, b);
+	MoveTo2D(x, y);
+	for(x; x < 50.0; x += xInc)
+	{
+		DrawTo2D(pixmap, colors::BLUE, x, eq3(x, -1, b));
+		MoveTo2D(x, eq3(x, -1, b));
+	}
+	
+	
+}
