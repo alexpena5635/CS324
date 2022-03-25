1fb856625b459602ac0b5494ed634a89cecbf0c4
alexpena5635 <alexpena5635@gmail.com>
2022-03-25 10:58:14 -0700

    Chapter 5 Complete - Sphere

--- raytracing_tut/main.cpp
+++ raytracing_tut/main.cpp
@@ -15,7 +15,18 @@
 #include "color.h"
 #include "ray.h"
 
+bool hit_sphere(const Point3& center, double radius, const Ray& r) {
+    Vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 Color ray_color(const Ray& r) {
+    if (hit_sphere(Point3(0,0,-1), 0.5, r))
+        return Color(1, 0, 0);
     Vec3 unit_dir = unit_vector(r.direction());
     auto t = 0.5*(unit_dir.y() + 1.0);
 
@@ -25,8 +36,22 @@ Color ray_color(const Ray& r) {
 int main()
 {
     // Image size
-    const int IMAGE_WIDTH  = 500;
-    const int IMAGE_HEIGHT = 500;
+    const auto ASPECT_RATIO = 16.0 / 9.0;
+    const int IMAGE_WIDTH = 600;
+    const int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = ASPECT_RATIO * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = Point3(0, 0, 0);
+    auto horizontal = Vec3(viewport_width, 0, 0);
+    auto vertical = Vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
+
+    // Render
 
     std::fstream savefile("image.ppm", std::fstream::out);
 
@@ -46,7 +71,11 @@ int main()
             // int ib = static_cast<int>(255.999 * b);
 
             // savefile << ir << ' ' << ig << ' ' << ib << '\n';
-            Color pixel_color(double(i)/(IMAGE_WIDTH-1), double(j)/(IMAGE_HEIGHT-1), 0.25);
+            // Color pixel_color(double(i)/(IMAGE_WIDTH-1), double(j)/(IMAGE_HEIGHT-1), 0.25);
+            auto u = double(i) / (IMAGE_WIDTH-1);
+            auto v = double(j) / (IMAGE_HEIGHT-1);
+            Ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            Color pixel_color = ray_color(r);
             write_color(savefile, pixel_color);
         }
     }
