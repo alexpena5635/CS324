510dbb2eec2f0a44c6073bdc03cb50c69c4189c0
Alex Pena <alexpena5635@gmail.com>
2022-03-29 20:16:46 -0700

    Cleaned up colors.h

--- assign3/colors.h
+++ assign3/colors.h
@@ -1,3 +1,13 @@
+/* 
+ * colors.h - Starter code from lecture notes.
+ * Defines a struct for rgb values, and a relevant namespace
+ * 
+ * Alex Peña
+ * CS 324
+ * Assignment 3 - 3D
+ * 03/29/2022
+ */
+
 #ifndef _COLORS_H_
 #define _COLORS_H_
 
@@ -10,11 +20,11 @@ struct color {
 };
 
 namespace colors {
-	constexpr color BLACK{   0,   0,   0 };
-	constexpr color WHITE{ 255, 255, 255 };
-	constexpr color RED{   255,   0,   0 };
-	constexpr color GREEN{   0, 255,   0 };
-	constexpr color BLUE{    0,   0, 255 };
+	constexpr color BLACK	{   0,   	0,		0 	};
+	constexpr color WHITE	{	255, 	255,	255 };
+	constexpr color RED		{   255,	0,		0 	};
+	constexpr color GREEN	{   0, 		255,	0 	};
+	constexpr color BLUE	{   0,   	0, 		255 };
 }
 
 #endif
