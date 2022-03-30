791dbc09229775aae30d36f7c20bd73ddfadf2ab
Alex Pena <alexpena5635@gmail.com>
2022-03-29 20:46:59 -0700

    Refactored and documented vec2.h vec3.h

--- assign3/vec2.h
+++ assign3/vec2.h
@@ -1,12 +1,12 @@
-/*
-*   vec2.h
-*   (Modified from code in: Ray Tracing in One Weekend)
-*   https://raytracing.github.io/books/RayTracingInOneWeekend.html#thevec3class 
-*
-*   Alex Peña
-*   CS324 Assignment 3
-* 
-*/
+/* 
+ * vec2.h (Modified from code in: Ray Tracing in One Weekend)
+ * Defines a 2D vector class, and operations on that vector
+ * 
+ * Alex Peña
+ * CS 324
+ * Assignment 3 - 3D
+ * 03/29/2022
+ */
 
 #ifndef VEC2_H_
 #define VEC2_H_
@@ -16,53 +16,53 @@
 
 class Vec2 {
     private:
-        double v[3]; // 3 length array. 2D points x,y + 1 for homogenous
+        double v[3];     // 3 length array. 2D points x,y + 1 for homogenous
 
     protected:
-        bool homogenous;
+        bool homogenous; // True when treating Vec2 as a homogenous vector
 
     public:
-        // Constructors
-        Vec2() : v{0.0, 0.0, 1.0} {homogenous = true;}
-        Vec2(double v0, double v1, double h=1, bool homog=true) : v{v0, v1, h} {homogenous = homog;}
+        /* Constructors */
+        Vec2()  : v{0.0, 0.0, 1.0}  { homogenous = true; }
+        Vec2(double v0, double v1, double h=1, bool homog=true) 
+                : v{v0, v1, h}      { homogenous = homog; }
 
-        // Getters
-        double x() const {return v[0];}
-        double y() const {return v[1];}
-        virtual double h() const {return v[2];}
+        /*  Getters  */
+        double x()          const { return v[0]; }
+        double y()          const { return v[1]; }
+        virtual double h()  const { return v[2]; } // homog. is different for 3D. Override later.
 
-        // Operator overloads
 
-        Vec2 operator-() const {  // Negate
+        /* Operator overloads */
+        Vec2 operator-() const {
+            // Only negate 'h' if homogenous
             return (
                 homogenous ? Vec2(-v[0], -v[1], -v[2]) : Vec2(-v[0], -v[1], v[2])
             );
-            // return Vec2(-v[0], -v[1], -v[2]);
         } 
 
-        double operator[](int i) const { return v[i]; } // Return constant of value 
-        double& operator[](int i) { return v[i]; } // Return reference to value
+        double  operator[](int i) const { return v[i]; } // Return constant of value 
+        double& operator[](int i)       { return v[i]; } // Return reference to value
         
-        Vec2& operator+=(const Vec2 &vec) { // Add passed vector to self
+        Vec2& operator+=(const Vec2 &vec) {
             v[0] += vec.v[0];
             v[1] += vec.v[1];
-            v[2] += (homogenous ? vec.v[2] : 0);
+            v[2] += (homogenous ? vec.v[2] : 0);        // Add 'h' if homog.
             return *this;
         }
 
-        Vec2& operator*=(const double t) { // Multiply self by passed number
+        Vec2& operator*=(const double t) {
             v[0] *= t;
             v[1] *= t;
-            v[2] *= (homogenous ? t : 1);
+            v[2] *= (homogenous ? t : 1);               // Subtract 'h' if homog
             return *this;
         }
 
-        Vec2& operator/=(const double t) { // Divide self by passed number
+        Vec2& operator/=(const double t) {
             return *this *= 1/t;
         }
 
-        // Utility member functions / methods
-
+        /* Utility member functions/methods */
         void set(double x, double y, double h=1, bool homog=true) {
             v[0] = x;
             v[1] = y;
@@ -70,22 +70,24 @@ class Vec2 {
             homogenous = homog;
         }
 
-        double magnitude() const { // Calcuate the magnitude of the vector
+        double magnitude() const {
             return std::sqrt(lengthSquared());
         }
 
         virtual double lengthSquared() const { // Square each component of the vector
-            return v[0]*v[0] + v[1]*v[1] + (homogenous ? v[2]*v[2] : 0);
+            return v[0]*v[0] 
+                + v[1]*v[1] 
+                + (homogenous ? v[2]*v[2] : 0); // Square 'h', add, if homog.
         }
 
-        bool Homog() const {return homogenous;}
-        void setHomog(bool set) {homogenous=set;}
+        bool Homog() const      { return homogenous; }
+        void setHomog(bool set) { homogenous = set;  }
 };
 
-// Type alias
+/* Type alias */
 typedef Vec2 Point2;
 
-// Vector 3 utility functions
+/* Vector2 utility functions */
 inline std::ostream& operator<<(std::ostream &out, const Vec2 &v) {
     return out << "x: " << v.x() << " y: " << v.y() << " h: " << v.h();
 }
@@ -117,10 +119,10 @@ inline Vec2 operator/(const Vec2 &v, double t) {
 inline double dot(const Vec2 &u, const Vec2 &v) {
     return  u.x() * v.x()
         +   u.y() * v.y()
-        +   (u.Homog() ? u.h() * v.h() : 0);
+        +   (u.Homog() ? u.h() * v.h() : 0);        // Only dot 'h' if homog.
 }
 
-inline double cross(const Vec2 &u, const Vec2 &v) { //2d cross product is just a scalar
+inline double cross(const Vec2 &u, const Vec2 &v) { // 2D cross product is just a scalar.
     return u.x() * v.y() - u.y() * v.x();
 }
 
--- assign3/vec3.h
+++ assign3/vec3.h
@@ -1,12 +1,12 @@
-/*
-*   vec3.h
-*   (Modified from code in: Ray Tracing in One Weekend)
-*   https://raytracing.github.io/books/RayTracingInOneWeekend.html#thevec3class 
-*
-*   Alex Peña
-*   CS324 Assignment 3
-* 
-*/
+/* 
+ * vec3.h (Modified from code in: Ray Tracing in One Weekend)
+ * Defines a 3D vector class, and operations on that vector
+ * 
+ * Alex Peña
+ * CS 324
+ * Assignment 3 - 3D
+ * 03/29/2022
+ */
 
 #ifndef VEC3_H_
 #define VEC3_H_
@@ -18,61 +18,58 @@
 class Vec3 : public Vec2 {
     private:
         double v[4]; // 4 length array. 3D points x,y,z + 1 for homogenous
-
-    // We inherit...
     /* 
     protected: 
         bool homogenous;
     */
-
     public:
-        // Constructors
-        Vec3() : v{0.0, 0.0, 0.0, 1} {homogenous = true;}
-        Vec3(double v0, double v1, double v2, double h=1, bool homog=true) : v{v0, v1, v2, h} {homogenous = homog;}
+        /* Constructors */
+        Vec3()  : v{0.0, 0.0, 0.0, 1}   { homogenous = true; }
+        Vec3(double v0, double v1, double v2, double h=1, bool homog=true) 
+                : v{v0, v1, v2, h}      { homogenous = homog; }
 
-        // Getters
-        // Inheriting...
+        /*  Getters  */
         /*
-        double x() const {return v[0];}
-        double y() const {return v[1];}
+        double x()          const { return v[0]; }
+        double y()          const { return v[1]; }
         */
-        double z() const {return v[2];}
-        double h() const override {return v[3];}
+        double z()          const { return v[2]; }
+        double h() const override { return v[3]; }
 
-        // Operator overloads
-        Vec3 operator-() const { // Negate
+        /* Operator overloads */
+        Vec3 operator-() const {
+            // Only negate 'h' if homogenous
             return (
-                homogenous ? Vec3(-v[0], -v[1], -v[2], -v[3]) : Vec3(-v[0], -v[1], -v[2], v[3])
+                homogenous ? Vec3(-v[0], -v[1], -v[2], -v[3]) 
+                    : Vec3(-v[0], -v[1], -v[2], v[3])
             );
         }
-        // Inherits..
         /*
-        double operator[](int i) const { return v[i]; } // Return constant of value 
-        double& operator[](int i) { return v[i]; } // Return reference to value
+        double  operator[](int i) const { return v[i]; } // Return constant of value 
+        double& operator[](int i)       { return v[i]; } // Return reference to value
         */
         
-        Vec3& operator+=(const Vec3 &vec) { // Add passed vector to self
+        Vec3& operator+=(const Vec3 &vec) {
             v[0] += vec.v[0];
             v[1] += vec.v[1];
             v[2] += vec.v[2];
-            v[3] += (Homog() ? vec.v[3] : 0);
+            v[3] += (Homog() ? vec.v[3] : 0);            // Add 'h' if homog
             return *this;
         }
 
-        Vec3& operator*=(const double t) { // Multiply self by passed number
+        Vec3& operator*=(const double t) {
             v[0] *= t;
             v[1] *= t;
             v[2] *= t;
-            v[3] *= (Homog() ? t : 1);
+            v[3] *= (Homog() ? t : 1);                   // Subtract 'h' if homog
             return *this;
         }
 
-        Vec3& operator/=(const double t) { // Divide self by passed number
+        Vec3& operator/=(const double t) {
             return *this *= 1/t;
         }
 
-        // Utility member functions / methods
-
+        /* Utility member functions/methods */
         void set(double x, double y, double z, double h=1, bool homog=true) {
             v[0] = x;
             v[1] = y;
@@ -80,7 +77,7 @@ class Vec3 : public Vec2 {
             v[3] = h;
             homogenous = homog;
         }
-        // Inheriting...
+
         /*
         double magnitude() const { // Calcuate the magnitude of the vector
             return sqrt(length_squared());
@@ -88,19 +85,22 @@ class Vec3 : public Vec2 {
         */
 
         double lengthSquared() const override { // Square each component of the vector
-            return v[0]*v[0] + v[1]*v[1] + v[2]*v[2] + (homogenous ? v[3]*v[3] : 0);
+            return v[0]*v[0] 
+                + v[1]*v[1] 
+                + v[2]*v[2] 
+                + (homogenous ? v[3]*v[3] : 0);  // Square 'h', add, if homog.
         }
 
-        // Inheriting...
         /*
-        bool is_homog() const {return homogenous;}
+        bool Homog() const      { return homogenous; }
+        void setHomog(bool set) { homogenous = set;  }
         */
 };
 
-// Type alias
+/* Type alias */
 typedef Vec3 Point3;
 
-// Vector 3 utility functions
+/* Vector2 utility functions */
 inline std::ostream& operator<<(std::ostream &out, const Vec3 &v) {
     return out << "x: " << v.x() << " y: " << v.y() << " z: " << v.z() << " h: " << v.h();
 }
@@ -133,10 +133,10 @@ inline double dot(const Vec3 &u, const Vec3 &v) {
     return  u.x() * v.x()
         +   u.y() * v.y()
         +   u.z() * v.z()
-        +   (u.Homog() ? u.h() * v.h() : 0);
+        +   (u.Homog() ? u.h() * v.h() : 0);        // Only dot 'h' if homog.
 }
 
-inline Vec3 cross(const Vec3 &u, const Vec3 &v) {
+inline Vec3 cross(const Vec3 &u, const Vec3 &v) {   // 3D cross product.
     return Vec3(u.y() * v.z() - u.z() * v.y(),
                 u.z() * v.x() - u.x() * v.z(),
                 u.x() * v.y() - u.y() * v.x()
@@ -146,5 +146,4 @@ inline Vec3 cross(const Vec3 &u, const Vec3 &v) {
 inline Vec3 unitVector(Vec3 v){
     return v / v.magnitude();
 }
-
 #endif
